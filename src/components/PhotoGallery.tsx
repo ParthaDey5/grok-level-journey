0c56e30c233cd89ff0e9f9e15c862d349f748ffa
PD Developer <pd@example.com>
2026-06-07 15:19:56 +0530

    Day 9

--- src/components/PhotoGallery.tsx
+++ src/components/PhotoGallery.tsx
@@ -1,79 +1,92 @@
 import React, { useState, useEffect } from "react";
 
 function PhotoGallery() {
-  const [images, setImages] = useState([]);
-  const [page, setPage] = useState(1);
+    const [images, setImages] = useState([]);
+    const [page, setPage] = useState(1);
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedImg, setSelectedImg] = useState(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedImg, setSelectedImg] = useState(null);
 
-  const LIMIT = 9;
+    const LIMIT = 9;
 
-  function loadImages() {
-    const newImages = [];
+    function loadImages() {
+        const newImages = [];
 
-    for (let i = 0; i < LIMIT; i++) {
-      newImages.push(
-        `https://picsum.photos/seed/${page}_${i}/400/400`
-      );
+        for (let i = 0; i < LIMIT; i++) {
+            newImages.push(
+                `https://picsum.photos/seed/${page}_${i}/400/400`
+            );
+        }
+
+        setImages(prev =>
+            [...prev, ...newImages]
+        );
+
+        setPage(prev => prev + 1);
     }
 
-    setImages(prev => [...prev, ...newImages]);
-    setPage(prev => prev + 1);
-  }
+    useEffect(() => {
+        console.log(images);
 
-  useEffect(() => {
-    loadImages();
-  }, []);
+    }, [images]);
 
-  function openModal(src) {
-    setSelectedImg(src);
-    setIsModalOpen(true);
-  }
+    useEffect(() => {
+        loadImages();
+    }, []);
 
-  function closeModal() {
-    setIsModalOpen(false);
-    setSelectedImg(null);
-  }
+    function openModal(src) {
+        setSelectedImg(src);
+        setIsModalOpen(true);
+    }
 
-  return (
-    <>
-      <section className="gallery">
-        <h2>Inspiration Gallery</h2>
+    function closeModal() {
+        setIsModalOpen(false);
+        setSelectedImg(null);
+    }
 
-        <div className="grid">
-          {images.map((src, i) => (
-            <div key={i} className="img-wrapper">
-              <img
-                src={src}
-                loading="lazy"
-                alt=""
-                onClick={() => openModal(src)}
-              />
-            </div>
-          ))}
-        </div>
+    return (
+        <>
+            <section className="gallery">
+                <h2>Inspiration Gallery</h2>
 
-        <button onClick={loadImages}>
-          Load More
-        </button>
-      </section>
+                <div className="grid">
+                    {images.map((src, i) => (
+                        <div key={i} className="img-wrapper">
+                            <img
+                                src={src}
+                                loading="lazy"
+                                alt=""
+                                onClick={() => openModal(src)}
+                            />
+                        </div>
+                    ))}
+                </div>
 
-      {/* MODAL */}
-      {isModalOpen && (
-        <div className="modal" onClick={closeModal}>
-          <span className="close">&times;</span>
+                <button onClick={loadImages}>
+                    Load More
+                </button>
+            </section>
 
-          <img
-            className="modal-img"
-            src={selectedImg}
-            onClick={(e) => e.stopPropagation()}
-            alt=""
-          />
-        </div>
-      )}
-    </>
-  );
+            {/* MODAL */}
+            {isModalOpen && (
+                <div className="modal relative z-[999]" onClick={closeModal}>
+                    <span className="close cursor-pointer absolute top-5 right-5" onClick={closeModal}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fff" className="size-8">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                        </svg>
+
+                    </span>
+
+                    <img
+                        className="modal-img"
+                        src={selectedImg}
+                        onClick={(e) => e.stopPropagation()}
+                        alt=""
+                    />
+                </div>
+            )}
+        </>
+    );
 }
 
 export default PhotoGallery;
